56b058722530adbacf7112e2f75acbeeb57126f3
Yasmin C Souza <67839410+yasmincsouza@users.noreply.github.com>
2023-05-11 00:56:41 -0300

    novo jogo

---
+++ Basico/forca.py
@@ -0,0 +1,9 @@
+def jogar():
+    print("********************************")
+    print("***Bem vindo ao jogo de Forca***")
+    print("********************************", end="\n\n")
+
+    print("Fim de jogo!")
+
+if (__name__ == "__main__"): # Le o arquivo principal e roda ele
+    jogar() 
