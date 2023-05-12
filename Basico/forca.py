742b723fdde713cf5cf0bd6c148859a7cb4519fc
Yasmin C Souza <67839410+yasmincsouza@users.noreply.github.com>
2023-05-11 22:23:22 -0300

    adicionado formatações string

--- Basico/forca.py
+++ Basico/forca.py
@@ -3,6 +3,22 @@ def jogar():
     print("***Bem vindo ao jogo de Forca***")
     print("********************************", end="\n\n")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    # enquanto não enforcou e não acertou 
+    while(not enforcou and not acertou):
+        chute = input("Qual letra? ")
+        chute = chute.strip() # remover espaços em branco à direita e à esquerda da letra digitada
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+        print("Jogando...")
+
     print("Fim de jogo!")
 
 if (__name__ == "__main__"): # Le o arquivo principal e roda ele
