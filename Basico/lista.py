8abffbe7945afa9f7a7046659a3b1cfb592dabab
Yasmin C Souza <67839410+yasmincsouza@users.noreply.github.com>
2023-05-12 23:12:25 -0300

    adicionado tuple

--- Basico/lista.py
+++ Basico/lista.py
@@ -1,8 +1,13 @@
-# valores = [] # como é criado a lista
-# print(type(valores))
+valores = [] # como é criado a lista
+print(type(valores))
 
 valores = [0,1,2,3,4]
 print(min(valores)) #valor minimo
 print(max(valores)) #valor maximo
 print(valores[3]) #retorna o valor pelo indice
 print(len(valores))
+
+# tuple é uma lista imutável
+dias = ("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado") # como é criado uma tupla
+print(type(dias))
+dias.append("Sábado2") # não é possivel adicionar, nem remover elementos.
