5ef1c339fc1ec9c3b25c116cf29b4491fd4db600
Yasmin C Souza <67839410+yasmincsouza@users.noreply.github.com>
2023-05-13 00:40:05 -0300

    modificador b adicionado

--- Basico/arquivo.py
+++ Basico/arquivo.py
@@ -8,3 +8,16 @@ arquivo = open("palavras.txt", "a")
 print(arquivo.write("morango\n"))
 print(arquivo.write("manga\n"))
 
+# o modificador b que devemos utilizar quando queremos 
+# trabalhar no modo binário. Para abrir uma imagem devemos usar
+imagem = open("foto.jpg", "rb")
+
+# arquivo copia.py
+# cria uma cópia de uma imagem
+logo = open('python-logo.png', 'rb')
+data = logo.read()
+logo.close()
+
+logo2 = open('python-logo2.png', 'wb')
+logo2.write(data)
+logo2.close()
