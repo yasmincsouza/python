04f994b858ad5002eb6dafcab72e9c267ada4652
Yasmin C Souza <67839410+yasmincsouza@users.noreply.github.com>
2023-05-13 00:37:38 -0300

    comentarios adicionados

--- Basico/arquivo.py
+++ Basico/arquivo.py
@@ -1,3 +1,4 @@
+# w é para escrita / r é para leitura / a para adicionar a um arquivo pré existente
 arquivo = open("palavras.txt", "w") # cria um arquivo
 print(arquivo.write("banana\n"))
 print(arquivo.write("melancia\n"))
