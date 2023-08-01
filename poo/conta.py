a8f155529bf41bab8641f01041c479c3ef365ee4
Yasmin C Souza <67839410+yasmincsouza@users.noreply.github.com>
2023-08-01 15:54:54 -0300

    construtor adicionado

---
+++ poo/conta.py
@@ -0,0 +1,9 @@
+
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto ... {}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
