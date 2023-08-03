ee3a3fe150a4b0d0302e9c659f5dde0f33ed677d
Yasmin C Souza <67839410+yasmincsouza@users.noreply.github.com>
2023-08-03 15:48:54 -0300

    metodos adicionados

--- poo/conta.py
+++ poo/conta.py
@@ -1,9 +1,20 @@
 
 class Conta:
-
+    #Função construtora
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto ... {}".format(self))
+        #Atributos
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    #Metodos
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
