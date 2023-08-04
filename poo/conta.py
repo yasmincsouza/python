bb5f0f8f6c7160e4e08c7378ac2f141d5c5c7208
Yasmin C Souza <67839410+yasmincsouza@users.noreply.github.com>
2023-08-04 18:32:25 -0300

    metodos estaticos

--- poo/conta.py
+++ poo/conta.py
@@ -2,6 +2,10 @@
 #O encapsulamento refere-se ao ato de tornar os atributos privados e permitir 
 #seu acesso apenas por meio dos métodos da classe, usando o prefixo __
 
+#Getters e setters são usados para proteger seus dados, especialmente na criação de classes. 
+#Para cada instância de variável, um método getter retorna seu valor, 
+#enquanto um método setter o define ou atualiza.
+
 class Conta:
     # Função construtora
     # Self funciona como referencia
@@ -26,14 +30,21 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
 
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.destino(valor)
 
-    @property
+    @property # para getters
     def saldo(self):
         return self.__saldo
 
@@ -45,6 +56,10 @@ class Conta:
     def limite(self):
         return self.__limite
 
-    @limite.setter
+    @limite.setter # para setters
     def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod #Metodos estaticos 
+    def codigo_banco():
+        return "001"
