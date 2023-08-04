69dbdb35c2bebe6a7462762c425cdea4e02dfb57
Yasmin C Souza <67839410+yasmincsouza@users.noreply.github.com>
2023-08-04 17:43:46 -0300

    propriedade

--- poo/conta.py
+++ poo/conta.py
@@ -1,4 +1,7 @@
 
+#O encapsulamento refere-se ao ato de tornar os atributos privados e permitir 
+#seu acesso apenas por meio dos métodos da classe, usando o prefixo __
+
 class Conta:
     # Função construtora
     # Self funciona como referencia
@@ -25,3 +28,23 @@ class Conta:
 
     def saca(self, valor):
         self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.destino(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
