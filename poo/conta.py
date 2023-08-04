5552965a779b3686873a749c693ffb3b7949722e
Yasmin C Souza <67839410+yasmincsouza@users.noreply.github.com>
2023-08-04 15:42:17 -0300

    atributos privados

--- poo/conta.py
+++ poo/conta.py
@@ -1,20 +1,27 @@
 
 class Conta:
-    #Função construtora
+    # Função construtora
+    # Self funciona como referencia
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto ... {}".format(self))
-        #Atributos
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        # Atributos publicos
+        #self.numero = numero
+        #self.titular = titular
+        #self.saldo = saldo
+        #self.limite = limite
+
+        # Atributos privados
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     #Metodos
     def extrato(self):
-        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
